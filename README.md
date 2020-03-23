41524ad7fb38136931df1b5e2e3cf17eba04a258
katai5plate <katai5plate.sub@gmail.com>
2020-03-23 15:37:46 +0900

    add cls, temp mode

--- README.md
+++ README.md
@@ -6,16 +6,7 @@ RPG アツマールのコンテンツツリー子作品数をランキングか�
 
 - ニコニコ実況が増えるゲームはどんなゲームなのかを調べたい。
 - ランキング上位にも関わらず、YouTube 実況が多いゲームとニコニコ実況が多いゲームが混在している。
-- ということはランキングに入ってるゲームのコンテンツツリーを調べて集計すれば結果が得られる！
-
-## どうやって？
-
-こうすればコンテンツツリー子作品数が取れる
-
-1. `https://public.api.nicovideo.jp/v1/rpgtkool/ranking.json?_limit=[1-100]&rankingType=[daily|weekly|monthly|total]`
-2. `res.data.games[].id` から ID を取得
-3. `http://commons.nicovideo.jp/tree/[ID]`
-4. `(new DOMParser()).parseFromString(result, "text/html").querySelector("#ChildBox>h3>span.num").innerText.replace(/（(\d{1,})）/,"$1")`
+- ということはランキングに入ってるゲームのコンテンツツリーを調べて集計すれば結果が得られるはず！
 
 ## 使い方
 
@@ -27,17 +18,28 @@ cd analyze-atsumaru-tree-children
 yarn
 ```
 
-### 実行
+### スクリプト
+
+### ランキング集計
 
 ```
-node . [type] [limit]
+yarn start [type] [limit]
 ```
 
 - type: 取得するランキング
   - `daily`, `weekly`, `monthly`, `total` のどれかを指定する
 - limit: 何位まで取得するか
   - 1 ～ 100 の数字を入力する
+- 100 位まで取得する場合 10 分ぐらいかかります。
+
+### dist ディレクトリ全消し
+
+```
+yarn cls
+```
+
+- 引数指定なし
 
 ### 出力結果
 
-`dist/*.json` に JSON ファイルが吐き出されます。
+`dist` ディレクトリに JSON ファイルが吐き出されます。
--- index.js
+++ index.js
@@ -6,12 +6,25 @@ const step = require("./step");
 
 const [, , type, limit = 100] = process.argv;
 
+fs.mkdirp("dist");
+
 // 処理開始
 (async () => {
   // バリデーション
   const TYPES = ["daily", "weekly", "monthly", "total"];
   if (!TYPES.includes(type)) return `${TYPES.join(" | ")} ?`;
   if (limit < 1 || 100 < limit) return `limit is 1-100`;
+
+  const filename = `dist/${type}_${limit}_${moment().format(
+    "YYYYMMDDHHmmss"
+  )}.json`;
+  const output = (res, status) => {
+    fs.writeJSONSync(
+      filename,
+      { meta: { type, limit: Number(limit), status }, result: res },
+      { spaces: 2 }
+    );
+  };
 
   // アツマールランキング API から目ぼしいデータだけ取り出す
   const API_RANKING = `https://public.api.nicovideo.jp/v1/rpgtkool/ranking.json?_limit=${limit}&rankingType=${type}`;
@@ -46,10 +59,15 @@ const [, , type, limit = 100] = process.argv;
       height,
       releaseTime: toTimeText(releaseTime),
       upgradeTime: toTimeText(upgradeTime),
-      index
+      index,
+      childCount: null
     })
   );
   console.log("// success!");
+
+  // 途中結果を出力
+  output(games, "working");
+  console.log("// create:", filename);
 
   // コンテンツツリーページから子作品数を取り出す
   const child = await step(
@@ -65,13 +83,13 @@ const [, , type, limit = 100] = process.argv;
       );
     }
   );
-  return games.map((x, i) => ({ ...x, childCount: child[i] }));
-})().then(res => {
+
+  // 結合
+  const result = games.map((x, i) => ({ ...x, childCount: child[i] }));
+
   // 結果出力
-  fs.writeJSONSync(
-    `dist/${type}_${limit}_${moment().format("YYYYMMDDHHmmss")}.json`,
-    { meta: { type, limit: Number(limit) }, result: res },
-    { spaces: 2 }
-  );
-  console.log(res);
-});
+  output(result, "done");
+  console.log("// update:", filename);
+
+  return result;
+})().then(console.log);
